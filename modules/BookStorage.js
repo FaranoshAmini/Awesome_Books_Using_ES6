1c69d3760c59281b5edd213e5f98a62a839ab1a0
Faranosh  Amini <faranosh.amini4@gmail.com>
2022-09-28 23:26:38 +0430

    Fixed Linters error

--- modules/BookStorage.js
+++ modules/BookStorage.js
@@ -1,9 +1,8 @@
 export default class BookStorage {
-  static saveData (bookArray)  {
-    localStorage.setItem('bookData', JSON.stringify(bookArray));
+  static saveData(bookArray) {
+    localStorage.setItem("bookData", JSON.stringify(bookArray));
   }
-  
-  static getData()  {
-    return JSON.parse(localStorage.getItem('bookData'));
+  static getData() {
+    return JSON.parse(localStorage.getItem("bookData"));
   }
 }
--- modules/Books.js
+++ modules/Books.js
@@ -1,5 +1,5 @@
 export default class Books {
-  constructor(bookid, title, author)  {
+  constructor(bookid, title, author) {
     this.bookid = bookid;
     this.title = title;
     this.author = author;
