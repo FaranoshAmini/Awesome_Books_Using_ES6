b0711f5bf32865b8572ca800361e8970691185dc
Faranosh  Amini <faranosh.amini4@gmail.com>
2022-09-28 20:38:36 +0430

    Update BookStorage.js

--- modules/BookStorage.js
+++ modules/BookStorage.js
@@ -2,6 +2,7 @@ export default class BookStorage {
   static saveData (bookArray)  {
     localStorage.setItem('bookData', JSON.stringify(bookArray));
   }
+  
   static getData()  {
     return JSON.parse(localStorage.getItem('bookData'));
   }
